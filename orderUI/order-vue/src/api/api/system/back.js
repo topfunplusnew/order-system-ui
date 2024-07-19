3bc5523f2ffab3b2af326fdb572e994d2f639c30
天明 <1036498144@qq.com>
2024-07-19 10:59:02 +0800

    fix(JS):更换JS2

---
+++ orderUI/order-vue/src/api/api/system/back.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询订单备份信息列表
+export function listBack(query) {
+  return request({
+    url: '/system/back/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询订单备份信息详细
+export function getBack(id) {
+  return request({
+    url: '/system/back/' + id,
+    method: 'get'
+  })
+}
+
+// 新增订单备份信息
+export function addBack(data) {
+  return request({
+    url: '/system/back',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改订单备份信息
+export function updateBack(data) {
+  return request({
+    url: '/system/back',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除订单备份信息
+export function delBack(id) {
+  return request({
+    url: '/system/back/' + id,
+    method: 'delete'
+  })
+}
