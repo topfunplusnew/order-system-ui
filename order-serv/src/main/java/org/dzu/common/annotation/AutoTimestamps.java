3b3c47c0b0f5104019a96bbe01153ae12e5d8442
panxu682 <2396389935@qq.com>
2024-08-01 00:09:28 +0800

    票据承兑完善

--- order-serv/src/main/java/org/dzu/common/annotation/AutoTimestamps.java
+++ order-serv/src/main/java/org/dzu/common/annotation/AutoTimestamps.java
@@ -4,7 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 /*
-* 自动增加时间*/
+* 自动增加时间
+*
+* */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface AutoTimestamps {
