f28847da2ba0824fce2e36fcd015797fbe891570
天明 <1036498144@qq.com>
2024-07-31 20:52:37 +0800

    feat(商业票据银行承兑): 功能完成(还款缺)

---
+++ order-UI/src/api/tool/format.js
@@ -0,0 +1,3 @@
+export const formatTime = (date) => {
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+}
--- order-UI/src/views/system/bankacceptance/index.vue
+++ order-UI/src/views/system/bankacceptance/index.vue
@@ -1,173 +1,30 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="操作日期" prop="operateDate">
-        <el-input
-          v-model="queryParams.operateDate"
-          placeholder="请输入操作日期"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
+      <el-form-item label="操作日期" prop="issueDate">
+        <el-date-picker
+          v-model="queryParams.issueDate"
+          type="date"
+          placeholder="选择日期" value-format="yyyy-MM-dd">
+        </el-date-picker>
       </el-form-item>
       <el-form-item label="票据号码" prop="billNo">
         <el-input
           v-model="queryParams.billNo"
           placeholder="请输入票据号码"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="出票日期" prop="issueDate">
-        <el-input
-          v-model="queryParams.issueDate"
-          placeholder="请输入出票日期"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="到期日期" prop="dueDate">
-        <el-input
-          v-model="queryParams.dueDate"
-          placeholder="请输入到期日期"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="我方承兑账户" prop="billAccount">
-        <el-input
-          v-model="queryParams.billAccount"
-          placeholder="请输入我方承兑账户"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="票据日期" prop="billDate">
-        <el-input
-          v-model="queryParams.billDate"
-          placeholder="请输入票据日期"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="事由" prop="reason">
-        <el-input
-          v-model="queryParams.reason"
-          placeholder="请输入事由"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="票据金额" prop="billAmount">
-        <el-input
-          v-model="queryParams.billAmount"
-          placeholder="请输入票据金额"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="贴息点数" prop="inDiscountPoints">
-        <el-input
-          v-model="queryParams.inDiscountPoints"
-          placeholder="请输入贴息点数"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="贴息金额" prop="inDiscountAmount">
-        <el-input
-          v-model="queryParams.inDiscountAmount"
-          placeholder="请输入贴息金额"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="票据种类" prop="billCategory">
-        <el-input
-          v-model="queryParams.billCategory"
-          placeholder="请输入票据种类"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="来源" prop="origin">
-        <el-input
-          v-model="queryParams.origin"
-          placeholder="请输入来源"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="背书人" prop="endorser">
-        <el-input
-          v-model="queryParams.endorser"
-          placeholder="请输入背书人"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="被背书人" prop="endorsee">
-        <el-input
-          v-model="queryParams.endorsee"
-          placeholder="请输入被背书人"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="背书事由" prop="endorseReason">
-        <el-input
-          v-model="queryParams.endorseReason"
-          placeholder="请输入背书事由"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="备注" prop="comments">
-        <el-input
-          v-model="queryParams.comments"
-          placeholder="请输入备注"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="添加时间" prop="addtime">
-        <el-input
-          v-model="queryParams.addtime"
-          placeholder="请输入添加时间"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="操作人员ID" prop="userId">
-        <el-input
-          v-model="queryParams.userId"
-          placeholder="请输入操作人员ID"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="操作人员姓名" prop="UserName">
-        <el-input
-          v-model="queryParams.UserName"
-          placeholder="请输入操作人员姓名"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="删除标记" prop="delFlag">
-        <el-input
-          v-model="queryParams.delFlag"
-          placeholder="请输入删除标记"
-          clearable
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <!--  <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>-->
       </el-form-item>
     </el-form>
 
     <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">刷新</el-button>
+      </el-col>
       <el-col :span="1.5">
         <el-button
           type="primary"
@@ -179,47 +36,47 @@
         >新增
         </el-button>
       </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:bankAcceptance:edit']"
-        >修改
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['system:bankAcceptance:remove']"
-        >删除
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['system:bankAcceptance:export']"
-        >导出
-        </el-button>
-      </el-col>
+      <!--      <el-col :span="1.5">-->
+      <!--        <el-button-->
+      <!--          type="success"-->
+      <!--          plain-->
+      <!--          icon="el-icon-edit"-->
+      <!--          size="mini"-->
+      <!--          :disabled="single"-->
+      <!--          @click="handleUpdate"-->
+      <!--          v-hasPermi="['system:bankAcceptance:edit']"-->
+      <!--        >修改-->
+      <!--        </el-button>-->
+      <!--      </el-col>-->
+      <!--      <el-col :span="1.5">-->
+      <!--        <el-button-->
+      <!--          type="danger"-->
+      <!--          plain-->
+      <!--          icon="el-icon-delete"-->
+      <!--          size="mini"-->
+      <!--          :disabled="multiple"-->
+      <!--          @click="handleDelete"-->
+      <!--          v-hasPermi="['system:bankAcceptance:remove']"-->
+      <!--        >删除-->
+      <!--        </el-button>-->
+      <!--      </el-col>-->
+      <!--      <el-col :span="1.5">-->
+      <!--        <el-button-->
+      <!--          type="warning"-->
+      <!--          plain-->
+      <!--          icon="el-icon-download"-->
+      <!--          size="mini"-->
+      <!--          @click="handleExport"-->
+      <!--          v-hasPermi="['system:bankAcceptance:export']"-->
+      <!--        >导出-->
+      <!--        </el-button>-->
+      <!--      </el-col>-->
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-horizontal-scroll="'always'" v-loading="loading" :data="bankAcceptanceList"
               @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center"/>
+      <!--  <el-table-column type="selection" width="55" align="center"/>-->
       <el-table-column label="id" align="center" prop="id"/>
       <el-table-column label="操作日期" align="center" prop="operateDate"/>
       <el-table-column label="票据号码" align="center" prop="billNo"/>
@@ -238,15 +95,15 @@
       <el-table-column label="被背书人" align="center" prop="endorsee"/>
       <el-table-column label="背书事由" align="center" prop="endorseReason"/>
       <el-table-column label="备注" align="center" prop="comments"/>
-      <el-table-column label="添加时间" align="center" prop="addtime"/>
-      <el-table-column label="操作人员ID" align="center" prop="userId"/>
-      <el-table-column label="操作人员姓名" align="center" prop="UserName"/>
-      <el-table-column label="删除标记" align="center" prop="delFlag"/>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <!--      <el-table-column label="添加时间" align="center" prop="addtime"/>-->
+      <!--      <el-table-column label="操作人员ID" align="center" prop="userId"/>-->
+      <!--      <el-table-column label="操作人员姓名" align="center" prop="UserName"/>-->
+      <!--      <el-table-column label="删除标记" align="center" prop="delFlag"/>-->
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="150px" fixed="right">
         <template slot-scope="scope">
           <el-button
             size="mini"
-            type="text"
+            type="primary"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['system:bankAcceptance:edit']"
@@ -254,7 +111,7 @@
           </el-button>
           <el-button
             size="mini"
-            type="text"
+            type="danger"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['system:bankAcceptance:remove']"
@@ -323,18 +180,18 @@
         <el-form-item label="备注" prop="comments">
           <el-input v-model="form.comments" placeholder="请输入备注"/>
         </el-form-item>
-        <el-form-item label="添加时间" prop="addtime">
-          <el-input v-model="form.addtime" placeholder="请输入添加时间"/>
-        </el-form-item>
-        <el-form-item label="操作人员ID" prop="userId">
-          <el-input v-model="form.userId" placeholder="请输入操作人员ID"/>
-        </el-form-item>
-        <el-form-item label="操作人员姓名" prop="UserName">
-          <el-input v-model="form.UserName" placeholder="请输入操作人员姓名"/>
-        </el-form-item>
-        <el-form-item label="删除标记" prop="delFlag">
-          <el-input v-model="form.delFlag" placeholder="请输入删除标记"/>
-        </el-form-item>
+        <!--        <el-form-item label="添加时间" prop="addtime">-->
+        <!--          <el-input v-model="form.addtime" placeholder="请输入添加时间"/>-->
+        <!--        </el-form-item>-->
+        <!--        <el-form-item label="操作人员ID" prop="userId">-->
+        <!--          <el-input v-model="form.userId" placeholder="请输入操作人员ID"/>-->
+        <!--        </el-form-item>-->
+        <!--        <el-form-item label="操作人员姓名" prop="UserName">-->
+        <!--          <el-input v-model="form.UserName" placeholder="请输入操作人员姓名"/>-->
+        <!--        </el-form-item>-->
+        <!--        <el-form-item label="删除标记" prop="delFlag">-->
+        <!--          <el-input v-model="form.delFlag" placeholder="请输入删除标记"/>-->
+        <!--        </el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -352,6 +209,7 @@ import {
   addBankAcceptance,
   updateBankAcceptance
 } from "@/api/system/bankAcceptance";
+import {formatTime} from "@/api/tool/format";
 
 export default {
   name: "BankAcceptance",
@@ -457,11 +315,15 @@ export default {
     /** 搜索按钮操作 */
     handleQuery() {
       this.queryParams.pageNum = 1;
+      //格式化时间
+      // const time = new Date(this.queryParams.issueDate)
+      // this.queryParams.issueDate = formatTime(time)
       this.getList();
     },
     /** 重置按钮操作 */
     resetQuery() {
       this.resetForm("queryForm");
+      // this.queryParams.issueDate = ''
       this.handleQuery();
     },
     // 多选框选中数据
