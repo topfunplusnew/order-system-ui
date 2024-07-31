b7239698e2047f48577f356b544c0225adea1990
kysand-985 <675762677@qq.com>
2024-07-31 10:45:44 +0800

    设置storeHouse和fleet的delFlag，addtime，updateTime，userId为空

--- order-UI/src/views/system/storehouse/index.vue
+++ order-UI/src/views/system/storehouse/index.vue
@@ -237,6 +237,10 @@ export default {
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.form.id != null) {
+            this.from.delFlag=null;
+            this.form.addtime=null;
+            this.form.updateTime=null;
+            this.form.userId=null;
             updateStoreHouse(this.form).then(response => {
               this.$modal.msgSuccess("修改成功");
               this.open = false;
