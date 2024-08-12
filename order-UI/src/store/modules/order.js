c60d7eac9cabe61abf18543847deb8db7468040e
天明 <1036498144@qq.com>
2024-08-12 15:12:10 +0800

    feat(订单添加/订单详情): 支持删除订单详情item

--- order-UI/src/store/modules/order.js
+++ order-UI/src/store/modules/order.js
@@ -13,7 +13,11 @@ const mutations = {
   ADD_ORDER_ITEM_LIST(state, orderItem) {
     state.orderItemList.push(orderItem)
   },
-  //修改的mutation
+  //删除订单详情
+  DELETE_ORDER_ITEM(state, index) {
+    state.orderItemList.splice(index, 1)
+  },
+  //修改订单详情
   CHANGE_ORDER_ITEM(state, orderItemInfo) {
     //组装orderItem对象 剔除index属性
     const {index, ...orderItem} = orderItemInfo;
@@ -25,7 +29,8 @@ const mutations = {
     }
   },
 
-  //
+
+  //修改订单列表
   CHANGE_ORDER_LIST(state, orderList) {
     state.orderList = orderList
   }
@@ -33,22 +38,28 @@ const mutations = {
 }
 
 const actions = {
+  //1.订单详情
   //添加
   addOrderItemList({commit}, orderItem) {
     commit('ADD_ORDER_ITEM_LIST', orderItem)
   },
-  //修改
+  //删除
+  deleteOrderItem({commit}, index) {
+    commit('DELETE_ORDER_ITEM', index)
+  },
   //action只能传递一个参数
   changeOrderItem({commit}, orderItemInfo) {
     commit('CHANGE_ORDER_ITEM', orderItemInfo)
   },
 
+
+  //2.订单列表
   //查询订单列表 赋值
   getOrderList({commit}) {
     listGoodsOrder().then(res => {
       commit('CHANGE_ORDER_LIST', res.rows)
     })
-  }
+  },
 }
 
 export default {
