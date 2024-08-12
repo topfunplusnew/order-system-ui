57c2098777733a2ba5d0ef45ab15e18e28a2ff1d
天明 <1036498144@qq.com>
2024-08-12 14:03:19 +0800

    feat(借出款信息): 修改BUG

--- order-serv/src/main/java/org/dzu/system/domain/LendMoney.java
+++ order-serv/src/main/java/org/dzu/system/domain/LendMoney.java
@@ -35,7 +35,7 @@ public class LendMoney extends BaseEntity {
     @Excel(name = "借出款编号")
     @Length(max = 50, message = "借出款编号的字符长度不允许超过50")
     @TableField("futuresNO")
-    @NotNull(message = "借出款编号不能为空")
+//    @NotNull(message = "借出款编号不能为空")
     private String futuresNO;
 
     /**
@@ -186,7 +186,9 @@ public class LendMoney extends BaseEntity {
         this.checkState = checkState;
     }
 
-    /** 额外字段 */
+    /**
+     * 额外字段
+     */
     @TableField(exist = false)
     private String checkState;
     // Getters and Setters
