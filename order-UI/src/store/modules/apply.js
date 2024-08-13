b7a866776fc9e2bed195ec2a1e6020a9169a3aec
天明 <1036498144@qq.com>
2024-08-13 17:39:35 +0800

    feat(付款申请): 添加付款申请，审核信息

---
+++ order-UI/src/store/modules/apply.js
@@ -0,0 +1,31 @@
+//订单状态信息 由订单Item组件主动推入
+const state = {
+  checked: ''
+}
+
+const mutations = {
+  //使用的时候 传入一个状态 修改
+  SET_CHECKED(state, status) {
+    state.checked = status;
+  },
+  CLEAR_CHECKED(state) {
+    state.checked = '';
+  }
+}
+
+const actions = {
+  setChecked({commit}, status) {
+    commit('SET_CHECKED', status)
+  },
+  //重置标记
+  clearChecked({commit}) {
+    commit('CLEAR_CHECKED', status)
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
