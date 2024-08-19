6b770d1060876afd5ee988945f864da834d281ef
天明 <1036498144@qq.com>
2024-08-19 16:21:00 +0800

    fix(用户): 修改bug

--- order-UI/src/views/index.vue
+++ order-UI/src/views/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="app-container home">
+  <div>
+    <el-row style="margin-top: 200px">
+      <el-col :span="12" :offset="8">
+        <LoadingWarning/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import LoadingWarning from "@/components/LoadingWarning.vue";
+
 export default {
   name: "Index",
+  components: {LoadingWarning},
   data() {
-    return {
-
-    };
+    return {};
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
