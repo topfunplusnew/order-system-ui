b9dcb72743e6930e577bc4dabd26c03cc75043e7
Beauty <12331232+healthyandrich@user.noreply.gitee.com>
2024-09-08 23:54:02 +0800

    出库

--- order-UI/src/utils/ruoyi.js
+++ order-UI/src/utils/ruoyi.js
@@ -91,7 +91,19 @@ export function addDateRange(params, dateRange, propName, otherParams) {
   }
   return search;
 }
-
+// export function adDateRange(params, dateRange, propName) {
+//   let search = params;
+//   search.params = typeof (search.params) === 'object' && search.params !== null && !Array.isArray(search.params) ? search.params : {};
+//   dateRange = Array.isArray(dateRange) ? dateRange : [];
+//   if (typeof (propName) === 'undefined') {
+//     search.params['beginTime'] = dateRange[0];
+//     search.params['endTime'] = dateRange[1];
+//   } else {
+//     search.params['begin' + propName] = dateRange[0];
+//     search.params['end' + propName] = dateRange[1];
+//   }
+//   return search;
+// }
 // 回显数据字典
 export function selectDictLabel(datas, value) {
   if (value === undefined) {
