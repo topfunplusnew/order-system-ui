942559ea113d41cd5605e0e10b1fd06d07045c5b
天明 <1036498144@qq.com>
2024-09-29 19:59:54 +0800

    ```简化并更新vue.config.js配置
    在order-system/order-UI项目中，精简了vue.config.js文件中的注释和设置。调整包括移除不必要的eslint检测，关闭生产环境的source map，以及更新静态资源的输出目录。此外，还对webpack-dev-server的代理配置进行了清理，以增强可读性和维护性。
    ```

--- order-UI/src/app_test.js
+++ order-UI/src/app_test.js
@@ -1,17 +1,4 @@
-import {create} from 'jsondiffpatch'
-import {format} from "jsondiffpatch/formatters/html";
+const json = require('jsondiffpatch/formatters/html')
+const jsondiffpatch = require('jsondiffpatch')
 
-console.log(format)
-
-const disPatcher = create()
-console.log(disPatcher)
-const obj1 = {name: 'Alice', age: 30};
-const obj2 = {name: 'Bob', age: 35};
-
-
-const diff = disPatcher.diff(obj1, obj2)
-
-const patchedObj = disPatcher.patch(obj1, diff);
-
-const html = format(diff, obj1);
-console.log(html)
+console.log(jsondiffpatch)
