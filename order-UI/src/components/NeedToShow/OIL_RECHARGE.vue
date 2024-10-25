29f3976ce5cf9f3da205d8397ea511e21ef41745
天明 <1036498144@qq.com>
2024-10-25 23:28:14 +0800

    refactor(order-UI): 移除油卡充值信息中的银行开户名和银行账号字段
    -从 OIL_RECHARGE.vue组件中删除了银行开户名和银行账号的显示项
    -简化了油卡充值信息的展示内容，提高了用户体验

--- order-UI/src/components/NeedToShow/OIL_RECHARGE.vue
+++ order-UI/src/components/NeedToShow/OIL_RECHARGE.vue
@@ -16,8 +16,6 @@ export default {
       <el-descriptions-item label="加油卡卡号">{{ needToShowInfo.oilCardNo }}</el-descriptions-item>
       <el-descriptions-item label="充值类型">{{ needToShowInfo.rechargeType }}</el-descriptions-item>
       <el-descriptions-item label="充值时间">{{ needToShowInfo.rechargeDate }}</el-descriptions-item>
-      <el-descriptions-item label="银行开户名">{{ needToShowInfo.acountsName }}</el-descriptions-item>
-      <el-descriptions-item label="银行账号">{{ needToShowInfo.bankNo }}</el-descriptions-item>
       <el-descriptions-item label="充值人员姓名">{{ needToShowInfo.rechargeName }}</el-descriptions-item>
     </el-descriptions>
   </div>
