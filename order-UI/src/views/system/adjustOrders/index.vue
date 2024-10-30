fbc39901b9ce9202c645a439baa16cfe0afcf021
旭霓 <xuni1024@outlook.com>
2024-10-30 15:12:49 +0800

    refactoer(调整单): 移除v-if判断,改为直接从子表信息中推断

--- order-UI/src/views/system/adjustOrders/index.vue
+++ order-UI/src/views/system/adjustOrders/index.vue
@@ -103,7 +103,6 @@
                        v-if="columns[2].visible"
                        width="200">
         <template #default="scope">
-          <el-row v-if="scope.row.supplierNames !== null">
             <el-row>
               <span v-for="(item,index) in getSupplierNames(scope.row.orderDetailList)" :key="index">
              <el-badge is-dot class="item">
@@ -113,7 +112,6 @@
           </el-badge>
           </span>
             </el-row>
-          </el-row>
           <el-row>
             <span v-if="scope.row.supplierNames === null">无</span>
           </el-row>
