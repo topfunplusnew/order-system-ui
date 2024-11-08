b94a5d09d82f89d3ed1c66087acb98ba3899f5aa
天明 <1036498144@qq.com>
2024-11-08 15:15:06 +0800

    feat(system): 添加车队运费报表功能
    - 新增车队运费报表页面，包含司机信息、运费日报、运费月报、运费年报等列
    - 实现时间范围筛选、搜索、刷新、导出等功能- 添加打印功能，支持自定义选择列
    - 新增 getF

--- order-UI/src/api/system/statement.js
+++ order-UI/src/api/system/statement.js
@@ -3,108 +3,118 @@ import request from "@/utils/request";
 
 // 运费报表
 export function getOrderFreight(query) {
-    return request({
-        url: '/statistics/orderfreightsummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/orderfreightsummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 社保和公积金缴纳金额
 export function getSocialInsuranceSummary(query) {
-    return request({
-        url: '/statistics/socialinsurancesummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/socialinsurancesummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 员工或外面公司在我公司借款管理台账 / 期货保证金台账
 export function getLendMoneySummary(query) {
-    return request({
-        url: '/statistics/lendMoneysummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/lendMoneysummary',
+    method: 'get',
+    params: query
+  })
 }
 
 export function getLendMoneySummary2(query) {
-    return request({
-        url: '/statistics/lendMoneysummary2',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/lendMoneysummary2',
+    method: 'get',
+    params: query
+  })
 }
 
 // 运费科目总台账
 export function getOrderFreightDetailSummary(query) {
-    return request({
-        url: '/statistics/orderfreightDetailsummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/orderfreightDetailsummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 客户报表
 export function getCompanySummary(query) {
-    return request({
-        url: '/statistics/companysummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/companysummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 供应商报表
 export function getSupplierSuymmary(query) {
-    return request({
-        url: '/statistics/suppliersuymmary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/suppliersuymmary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 贷款管理台账
 export function getBorrowedMoneySummary(query) {
-    return request({
-        url: '/statistics/borrowedMoneySummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/borrowedMoneySummary',
+    method: 'get',
+    params: query
+  })
 }
 
 
 // 费用科目汇总
 export function getSubjectSummary(query) {
-    return request({
-        url: '/statistics/subjectsummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/subjectsummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 获取发货列表
 export function getDeliveryList(query) {
-    return request({
-        url: '/statistics/getTodayOrderList',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/getTodayOrderList',
+    method: 'get',
+    params: query
+  })
 }
 
 // 资金日报表
 export function getBankAcountChange(query) {
-    return request({
-        url: '/statistics/bankAcountChangeSummary',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: '/statistics/bankAcountChangeSummary',
+    method: 'get',
+    params: query
+  })
 }
 
 // 获取客户每日发货次数
 export function getDailyOrderCount(query) {
-    return request({
-        url: 'statistics/dailyOrderCount',
-        method: 'get',
-        params: query
-    })
+  return request({
+    url: 'statistics/dailyOrderCount',
+    method: 'get',
+    params: query
+  })
+}
+
+
+// 获取车队运费报表
+export function getFleetFreightSummary(query) {
+  return request({
+    url: '/statistics/fleetfreightsummary',
+    method: 'get',
+    params: query
+  })
 }
