1cfbdfdaec9cfa4e27bde3d282344ff9fa8f177f
天明 <1036498144@qq.com>
2024-11-10 11:19:50 +0800

    完善奖励

--- order-UI/src/api/system/salessingorderincentivedetails.js
+++ order-UI/src/api/system/salessingorderincentivedetails.js
@@ -1,9 +1,10 @@
 import request from '@/utils/request'
 
 // 查询唱单制列表
+// todo 唱台和台阶的接口互换
 export function listSalessingorderincentivedetails(query) {
   return request({
-    url: '/system/salessingorderincentivedetails/list',
+    url: '/system/salesstepincentivedetails/list',
     method: 'get',
     params: query
   })
@@ -12,7 +13,7 @@ export function listSalessingorderincentivedetails(query) {
 // 查询唱单制详细
 export function getSalessingorderincentivedetails(id) {
   return request({
-    url: '/system/salessingorderincentivedetails/' + id,
+    url: '/system/salesstepincentivedetails/' + id,
     method: 'get'
   })
 }
@@ -20,7 +21,7 @@ export function getSalessingorderincentivedetails(id) {
 // 新增唱单制
 export function addSalessingorderincentivedetails(data) {
   return request({
-    url: '/system/salessingorderincentivedetails',
+    url: '/system/salesstepincentivedetails',
     method: 'post',
     data: data
   })
@@ -29,7 +30,7 @@ export function addSalessingorderincentivedetails(data) {
 // 修改唱单制
 export function updateSalessingorderincentivedetails(data) {
   return request({
-    url: '/system/salessingorderincentivedetails',
+    url: '/system/salesstepincentivedetails',
     method: 'put',
     data: data
   })
@@ -38,7 +39,55 @@ export function updateSalessingorderincentivedetails(data) {
 // 删除唱单制
 export function delSalessingorderincentivedetails(id) {
   return request({
-    url: '/system/salessingorderincentivedetails/' + id,
+    url: '/system/salesstepincentivedetails/' + id,
     method: 'delete'
   })
 }
+
+
+/**
+ * import request from '@/utils/request'
+ *
+ * // 查询台阶制列表
+ * export function listSalesstepincentivedetails(query) {
+ *   return request({
+ *     url: '/system/salesstepincentivedetails/list',
+ *     method: 'get',
+ *     params: query
+ *   })
+ * }
+ *
+ * // 查询台阶制详细
+ * export function getSalesstepincentivedetails(id) {
+ *   return request({
+ *     url: '/system/salesstepincentivedetails/' + id,
+ *     method: 'get'
+ *   })
+ * }
+ *
+ * // 新增台阶制
+ * export function addSalesstepincentivedetails(data) {
+ *   return request({
+ *     url: '/system/salesstepincentivedetails',
+ *     method: 'post',
+ *     data: data
+ *   })
+ * }
+ *
+ * // 修改台阶制
+ * export function updateSalesstepincentivedetails(data) {
+ *   return request({
+ *     url: '/system/salesstepincentivedetails',
+ *     method: 'put',
+ *     data: data
+ *   })
+ * }
+ *
+ * // 删除台阶制
+ * export function delSalesstepincentivedetails(id) {
+ *   return request({
+ *     url: '/system/salesstepincentivedetails/' + id,
+ *     method: 'delete'
+ *   })
+ * }
+ */
--- order-UI/src/api/system/salesstepincentivedetails.js
+++ order-UI/src/api/system/salesstepincentivedetails.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询台阶制列表
 export function listSalesstepincentivedetails(query) {
   return request({
-    url: '/system/salesstepincentivedetails/list',
+    url: '/system/salessingorderincentivedetails/list',
     method: 'get',
     params: query
   })
@@ -12,7 +12,7 @@ export function listSalesstepincentivedetails(query) {
 // 查询台阶制详细
 export function getSalesstepincentivedetails(id) {
   return request({
-    url: '/system/salesstepincentivedetails/' + id,
+    url: '/system/salessingorderincentivedetails/' + id,
     method: 'get'
   })
 }
@@ -20,7 +20,7 @@ export function getSalesstepincentivedetails(id) {
 // 新增台阶制
 export function addSalesstepincentivedetails(data) {
   return request({
-    url: '/system/salesstepincentivedetails',
+    url: '/system/salessingorderincentivedetails',
     method: 'post',
     data: data
   })
@@ -29,7 +29,7 @@ export function addSalesstepincentivedetails(data) {
 // 修改台阶制
 export function updateSalesstepincentivedetails(data) {
   return request({
-    url: '/system/salesstepincentivedetails',
+    url: '/system/salessingorderincentivedetails',
     method: 'put',
     data: data
   })
@@ -38,7 +38,56 @@ export function updateSalesstepincentivedetails(data) {
 // 删除台阶制
 export function delSalesstepincentivedetails(id) {
   return request({
-    url: '/system/salesstepincentivedetails/' + id,
+    url: '/system/salessingorderincentivedetails/' + id,
     method: 'delete'
   })
 }
+
+
+/**
+ * import request from '@/utils/request'
+ *
+ * // 查询唱单制列表
+ * // todo 唱台和台阶的接口互换
+ * export function listSalessingorderincentivedetails(query) {
+ *   return request({
+ *     url: '/system/salessingorderincentivedetails/list',
+ *     method: 'get',
+ *     params: query
+ *   })
+ * }
+ *
+ * // 查询唱单制详细
+ * export function getSalessingorderincentivedetails(id) {
+ *   return request({
+ *     url: '/system/salessingorderincentivedetails/' + id,
+ *     method: 'get'
+ *   })
+ * }
+ *
+ * // 新增唱单制
+ * export function addSalessingorderincentivedetails(data) {
+ *   return request({
+ *     url: '/system/salessingorderincentivedetails',
+ *     method: 'post',
+ *     data: data
+ *   })
+ * }
+ *
+ * // 修改唱单制
+ * export function updateSalessingorderincentivedetails(data) {
+ *   return request({
+ *     url: '/system/salessingorderincentivedetails',
+ *     method: 'put',
+ *     data: data
+ *   })
+ * }
+ *
+ * // 删除唱单制
+ * export function delSalessingorderincentivedetails(id) {
+ *   return request({
+ *     url: '/system/salessingorderincentivedetails/' + id,
+ *     method: 'delete'
+ *   })
+ * }
+ */
