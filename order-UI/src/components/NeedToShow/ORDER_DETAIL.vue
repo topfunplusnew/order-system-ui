d554f3f41f21c2821226f39dd261f9299ad0e0da
topfun <1036498144@qq.com>
2024-11-20 15:29:25 +0800

    refactor(system): 优化客户和供应商汇总表的期初和期末

--- order-UI/src/components/NeedToShow/ORDER_DETAIL.vue
+++ order-UI/src/components/NeedToShow/ORDER_DETAIL.vue
@@ -18,11 +18,11 @@ export default {
         <el-descriptions-item label="订单日期">{{ needToShowInfo.orderDate }}</el-descriptions-item>
         <el-descriptions-item label="供应商">{{ needToShowInfo.supplier }}</el-descriptions-item>
         <el-descriptions-item label="客户">{{ needToShowInfo.customer }}</el-descriptions-item>
-        <el-descriptions-item label="等级">{{ needToShowInfo.levelName }}</el-descriptions-item>
+        <el-descriptions-item label="级别名称">{{ needToShowInfo.levelName }}</el-descriptions-item>
         <el-descriptions-item label="计量单位">{{ needToShowInfo.countingUnit }}</el-descriptions-item>
-        <el-descriptions-item label="高度">{{ needToShowInfo.height }} m</el-descriptions-item>
-        <el-descriptions-item label="长度">{{ needToShowInfo.length }} mm</el-descriptions-item>
-        <el-descriptions-item label="宽度">{{ needToShowInfo.width }} mm</el-descriptions-item>
+        <el-descriptions-item label="高度">{{ needToShowInfo.height }}</el-descriptions-item>
+        <el-descriptions-item label="长度">{{ needToShowInfo.length }}</el-descriptions-item>
+        <el-descriptions-item label="宽度">{{ needToShowInfo.width }}</el-descriptions-item>
         <el-descriptions-item label="总片数">{{ needToShowInfo.pieces }}</el-descriptions-item>
         <el-descriptions-item label="每包片数">{{ needToShowInfo.piecesPerPack }}</el-descriptions-item>
         <el-descriptions-item label="总包数">{{ needToShowInfo.packs }}</el-descriptions-item>
@@ -40,7 +40,7 @@ export default {
         </el-descriptions-item>
         <el-descriptions-item label="总付款">{{ needToShowInfo.payments }} 元</el-descriptions-item>
         <el-descriptions-item label="误差">{{ needToShowInfo.erro }}</el-descriptions-item>
-        <el-descriptions-item label="吨数">{{ needToShowInfo.tonnage }} 吨</el-descriptions-item>
+        <el-descriptions-item label="吨数">{{ needToShowInfo.tonnage }}</el-descriptions-item>
         <el-descriptions-item label="陆运价">{{ needToShowInfo.landFreightPrice }} 元</el-descriptions-item>
         <el-descriptions-item label="陆运费">{{ needToShowInfo.landFreight }} 元</el-descriptions-item>
         <el-descriptions-item label="海运费">{{ needToShowInfo.seaFreight || '无' }} 元</el-descriptions-item>
@@ -54,7 +54,6 @@ export default {
         <el-descriptions-item label="仓库名称">{{ needToShowInfo.storeHouseName || '无' }}</el-descriptions-item>
         <el-descriptions-item label="物流利润">{{ needToShowInfo.logisticsProfit }} 元</el-descriptions-item>
         <el-descriptions-item label="客户佣金">{{ needToShowInfo.customerCommission }} 元</el-descriptions-item>
-        <el-descriptions-item label="调整标志">{{ needToShowInfo.isAdjusted ? '是' : '否' }}</el-descriptions-item>
         <el-descriptions-item label="调整订单号">{{ needToShowInfo.adjustOrderNo || '无' }}</el-descriptions-item>
         <el-descriptions-item label="调整日期">{{ needToShowInfo.adjustDate || '无' }}</el-descriptions-item>
         <el-descriptions-item label="备注">
