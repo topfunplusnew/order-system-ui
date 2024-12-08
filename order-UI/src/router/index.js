e8ad6560c6092c4d872a71304658829e5950f175
topfun <1036498144@qq.com>
2024-12-08 16:35:32 +0800

    refactor(router): 移除测试用的 base 路由前缀
    - 删除了正式环境中不需要的 '/orders/' 前缀
    - 将 base 路由设置为根路径 '/'

--- order-UI/src/router/index.js
+++ order-UI/src/router/index.js
@@ -170,7 +170,7 @@ Router.prototype.replace = function push(location) {
 };
 
 export default new Router({
-	base: '/orders/', // todo 这个在正式环境中要记得删除
+	base: '/',
 	mode: 'history', // 去掉url中的#
 	scrollBehavior: () => ({ y: 0 }),
 	routes: constantRoutes
