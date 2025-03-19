bd5e372e298d1e2c0c1734e0ff6a2689ec487954
topfun <1036498144@qq.com>
2025-03-19 16:58:49 +0800

    feat(order-form): 添加工厂佣金字段并更新相关属性
    - 在订单表单中添加工厂佣金字段- 将计提厂家返利金额的属性从 customerCommission 改为 factoryRebateAmount
    - 将计提厂家降价金额的属性从 customerCommission 改为 factoryDiscountAmount

--- order-UI/packages/order-system/src/views/dashboard/components/goodsOrder/OrderForm.vue
+++ order-UI/packages/order-system/src/views/dashboard/components/goodsOrder/OrderForm.vue
@@ -234,6 +234,7 @@ export default {
 			obj.storeID = '';
 			obj.logisticsProfit = '';
 			obj.customerCommission = '';
+			obj.factoryCommission = '';
 			obj.isAdjusted = 0;
 			obj.adjustOrderNo = '';
 			obj.adjustDate = '';
@@ -1082,12 +1083,12 @@ export default {
 						</template>
 					</el-table-column>
 					<!--          降价金额-->
-					<el-table-column label="计提厂家返利金额" prop="customerCommission" width="150">
+					<el-table-column label="计提厂家返利金额" prop="factoryRebateAmount" width="150">
 						<template #default="scope">
 							<el-input size="mini" v-model="scope.row.factoryRebateAmount" placeholder="请输入计提厂家返利金额" />
 						</template>
 					</el-table-column>
-					<el-table-column label="计提厂家降价金额" prop="customerCommission" width="150">
+					<el-table-column label="计提厂家降价金额" prop="factoryDiscountAmount" width="150">
 						<template #default="scope">
 							<el-input size="mini" v-model="scope.row.factoryDiscountAmount" placeholder="请输入计提厂家降价金额" />
 						</template>
