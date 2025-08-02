670d2f1bd0a3e888abe9f0d51e93d0c47c674b5c
张明 <125556714+Topfunplus@users.noreply.github.com>
2025-08-02 16:20:25 +0800

    修复订单的修改原因展示

--- order-UI/packages/order-system/src/views/dashboard/components/goodsOrder/OrderHistoryList.vue
+++ order-UI/packages/order-system/src/views/dashboard/components/goodsOrder/OrderHistoryList.vue
@@ -1,6 +1,13 @@
 <script>
+import { getGoodsOrder } from '@/api/system/goodsOrder';
+import DialogWrapper from '@/views/dashboard/components/common/DialogWrapper.vue';
+import { common_dialog } from '@/views/dashboard/mixins/common/common_dialog';
+import GOODS_ORDER from '@/components/NeedToShow/GOODS_ORDER.vue';
+
 export default {
 	name: 'OrderHistoryList',
+	components: { DialogWrapper },
+	mixins: [common_dialog],
 	props: {
 		goodsOrderList: {
 			type: Array,
@@ -14,7 +21,36 @@ export default {
 		return {};
 	},
 	methods: {
-		handleReject() {}
+		handleProcess() {},
+		handleReject() {},
+		// 查看订单详情
+		handleViewOrder(row) {
+			if (!row.id) {
+				this.$message.error('订单ID不存在');
+				return;
+			}
+
+			getGoodsOrder(row.id)
+				.then(res => {
+					if (!res.data) {
+						this.$message.error('暂无该条订单数据');
+						return;
+					}
+
+					this.openDialog(
+						GOODS_ORDER,
+						'订单信息',
+						'900px',
+						{
+							needToShowInfo: res.data
+						},
+						false
+					);
+				})
+				.catch(error => {
+					this.$message.error('获取订单信息失败: ' + error.message);
+				});
+		}
 	}
 };
 </script>
@@ -35,48 +71,6 @@ export default {
 				}
 			"
 		>
-			<el-table-column show-overflow-tooltip label="日期" align="center" prop="orderDate" />
-			<el-table-column show-overflow-tooltip label="客户" align="center" prop="customer" />
-			<el-table-column show-overflow-tooltip label="供应商" align="center" prop="supplierNames" width="200"></el-table-column>
-			<el-table-column show-overflow-tooltip label="陆运车牌" align="center" prop="landCarNo" />
-			<el-table-column show-overflow-tooltip label="陆运司机电话" align="center" prop="landDriverTel" width="100px" />
-			<el-table-column show-overflow-tooltip label="陆地司机姓名" align="center" prop="landDriverName" width="100px" />
-			<el-table-column show-overflow-tooltip label="总货款" align="center" prop="allPayments" width="100px"></el-table-column>
-			<el-table-column show-overflow-tooltip label="陆运费" align="center" prop="landFreight" width="100px" />
-			<!--      原为海运车牌号-->
-			<el-table-column show-overflow-tooltip label="海运柜号" align="center" prop="seaCarNo">
-				<template #default="scope">
-					{{ !scope.row.seaCarNo ? '无' : scope.row.seaCarNo }}
-				</template>
-			</el-table-column>
-			<el-table-column show-overflow-tooltip label="海运司机电话" align="center" prop="seaDriverTel" width="100px">
-				<template #default="scope">
-					{{ !scope.row.seaDriverTel ? '无' : scope.row.seaDriverTel }}
-				</template>
-			</el-table-column>
-			<!--      原为海运司机姓名-->
-			<el-table-column show-overflow-tooltip label="海运公司" align="center" prop="seaDriverName" width="100px">
-				<template #default="scope">
-					{{ !scope.row.seaDriverName ? '无' : scope.row.seaDriverTel }}
-				</template>
-			</el-table-column>
-			<el-table-column show-overflow-tooltip label="海运费" align="center" prop="seaFreight" width="100px" />
-			<el-table-column show-overflow-tooltip label="销售经理" align="center" prop="saleManager" />
-			<el-table-column show-overflow-tooltip label="车队" align="center" prop="fleet" />
-			<el-table-column show-overflow-tooltip label="录入员" align="center" prop="userName" width="120px" />
-			<el-table-column show-overflow-tooltip label="审核状态" align="center" prop="checkState" width="120"></el-table-column>
-			<el-table-column show-overflow-tooltip label="开票状态" align="center" prop="invoiceState" width="120px" />
-			<el-table-column show-overflow-tooltip label="客户是否开票" align="center" prop="customerIsInvoice" width="150px">
-				<template #default="scope">
-					{{ scope.row.customerIsInvoice === 1 ? '是' : '否' }}
-				</template>
-			</el-table-column>
-			<el-table-column show-overflow-tooltip label="供应商是否开票" align="center" prop="isSupplierInvoice" width="120px">
-				<template #default="scope">
-					{{ scope.row.isSupplierInvoice >= 1 ? '是' : '否' }}
-				</template>
-			</el-table-column>
-			<el-table-column show-overflow-tooltip label="备注" align="center" prop="comments" />
 			<el-table-column show-overflow-tooltip label="修改时间" align="center" prop="updateTime">
 				<template #default="scope">
 					{{ scope.row.tableEditMessage.modifyTime }}
@@ -94,7 +88,18 @@ export default {
 					{{ scope.row.tableEditMessage.userName }}
 				</template>
 			</el-table-column>
+
+			<el-table-column label="操作" align="center" width="120">
+				<template #default="scope">
+					<el-button size="mini" type="text" @click="handleViewOrder(scope.row)">查看订单</el-button>
+				</template>
+			</el-table-column>
 		</el-table>
+
+		<!-- 通用弹窗组件 -->
+		<div v-if="currentComponent">
+			<DialogWrapper :current-component="currentComponent" :dialog-visible="dialogVisible" />
+		</div>
 	</div>
 </template>
 
