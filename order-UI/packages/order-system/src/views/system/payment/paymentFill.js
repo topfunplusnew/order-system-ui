34a57ee0e52bf5184b252fa281513c043efb5441
topfun <ming10655@gmail.com>
2025-11-16 09:51:49 +0800

    更新paymentFill视图，新增对PUBLIC_DICT_TYPE的导入，并在员工类型逻辑中添加调试信息，以提升数据处理的准确性和便于调试。

--- order-UI/packages/order-system/src/views/system/payment/paymentFill.js
+++ order-UI/packages/order-system/src/views/system/payment/paymentFill.js
@@ -1,5 +1,5 @@
 // payment/index.vue所需的付款
-import { PAYMENT_TARGET_TYPE } from '../../../api/tool/enums';
+import { PAYMENT_TARGET_TYPE, PUBLIC_DICT_TYPE } from '../../../api/tool/enums';
 export var mixin_paymentindex_fill = {
 	data: function () {
 		return {
@@ -43,6 +43,7 @@ export var mixin_paymentindex_fill = {
 			this.form.otherBankNo = val.bankNo;
 			// 如果是员工，那么就从银行卡中取信息填充
 			if (this.form.companyType === PUBLIC_DICT_TYPE.EMPLOYEE) {
+				console.log(`val`, val);
 				this.form.companyId = val.companyId;
 				this.form.companyName = val.companyName;
 			}
