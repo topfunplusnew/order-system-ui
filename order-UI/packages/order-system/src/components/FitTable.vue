dd052ee01125621b4238f2bcb0b5af67d971acbe
2005 <2077879963@qq.com>
2025-11-18 16:52:25 +0800

    按照老系统调整列宽 使用插件来做

---
+++ order-UI/packages/order-system/src/components/FitTable.vue
@@ -0,0 +1,57 @@
+<!-- src/components/FitTable.vue -->
+<template>
+  <el-table
+      :data="tableData"
+      v-bind="$attrs"
+      class="fit-table"
+      @header-click="handleHeaderClick"
+  >
+    <!-- 组件内容 -->
+    <slot />
+  </el-table>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+import FitColumnPlugin from 'v-fit-columns';
+
+export default {
+  name: 'FitTable',
+  inheritAttrs: false,
+  props: {
+    tableData: {
+      type: Array,
+      default: () => []
+    },
+    fitColumns: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props) {
+    const tableRef = ref(null);
+
+    // 监听数据变化，重新计算列宽
+    watch(() => props.tableData, () => {
+      if (props.fitColumns && tableRef.value) {
+        FitColumnPlugin.resize(tableRef.value);
+      }
+    });
+
+    const handleHeaderClick = (column, event) => {
+      $emit('header-click', column, event);
+    };
+
+    return {
+      tableRef,
+      handleHeaderClick
+    };
+  }
+};
+</script>
+
+<style scoped>
+.fit-table {
+  width: 100%;
+}
+</style>
