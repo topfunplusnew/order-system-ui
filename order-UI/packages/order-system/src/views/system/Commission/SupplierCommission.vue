4528bfe93c72ebe3cf7f6c5e640bffa84638c5ee
张明 <ming10655@gmail.com>
2025-11-22 09:32:09 +0800

    修复佣金的按钮问题

--- order-UI/packages/order-system/src/views/system/Commission/SupplierCommission.vue
+++ order-UI/packages/order-system/src/views/system/Commission/SupplierCommission.vue
@@ -129,16 +129,19 @@
 						{{ scope.row.differenceReason || '填写差异原因' }}
 					</el-button>
 					<span v-else></span>
+					Ï
 				</template>
 			</el-table-column>
 
 			<!-- 操作列 -->
-			<el-table-column show-overflow-tooltip label="操作" align="center" width="400" class-name="small-padding fixed-width" fixed="right">
+			<el-table-column show-overflow-tooltip label="操作" align="center" width="350" class-name="small-padding fixed-width" fixed="right">
 				<template slot-scope="scope">
-					<el-button type="text" size="mini" @click="handleViewOrder(scope.row)">查看订单</el-button>
-					<el-button type="text" size="mini" @click="handleEdit(scope.row)">{{ scope.row.id ? '修改佣金信息' : '填写佣金信息' }}</el-button>
-					<el-button :disabled="scope.row.id === null" type="text" size="mini" @click="handleDelete(scope.row)">删除</el-button>
-					<el-button :disabled="scope.row.id === null" type="text" size="mini" @click="handleApplyPayment(scope.row)">申请付款</el-button>
+					<div>
+						<el-button type="text" size="mini" @click="handleViewOrder(scope.row)">查看订单</el-button>
+						<el-button type="text" size="mini" @click="handleEdit(scope.row)">{{ scope.row.id ? '修改佣金信息' : '填写佣金信息' }}</el-button>
+						<el-button :disabled="scope.row.id === null" type="text" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+						<el-button :disabled="scope.row.id === null" type="text" size="mini" @click="handleApplyPayment(scope.row)">申请付款</el-button>
+					</div>
 				</template>
 			</el-table-column>
 		</el-table>
