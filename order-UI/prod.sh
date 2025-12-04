d7dd2d55760dcbcebe26ef8126e18c15f03f329d
topfun <ming10655@gmail.com>
2025-12-04 22:45:38 +0800

    增强Docker配置，支持可选的网站目录挂载，确保网站文件的正确处理和路径设置

--- order-UI/prod.sh
+++ order-UI/prod.sh
@@ -1,4 +1,5 @@
 docker run -d \
   -p 40085:40085 \
   -v /opt/front:/opt/front \
+  -v /opt/front/html:/opt/front/html \
   registry.superslash.cn/order-system-ui:latest
--- order-UI/test.sh
+++ order-UI/test.sh
@@ -1,9 +1,10 @@
-docker run -d \
+sudo docker run -d \
   -p 40081:40081 \
   -e FRONTEND_PORT=40081 \
   -e BACKEND_PORT=60036 \
   -e HOST_IP=60.205.5.253 \
   -v /opt/front-test:/opt/front \
+  -v /opt/front-test/html:/opt/front/html \
   registry.superslash.cn/order-system-ui:latest
 
   # 60.205.5.253:60036 测试环境
