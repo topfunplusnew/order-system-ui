0a5b236921bb85e67fb7c7f9c1ab4c6279e0e1dc
2005 <2077879963@qq.com>
2025-12-22 19:15:58 +0800

    添加删除出库记录功能，并优化相关操作按钮布局

--- packages/order-system/src/views/system/giftOut/index.vue
+++ packages/order-system/src/views/system/giftOut/index.vue
@@ -71,7 +71,7 @@
 
 			<el-table-column v-if="columns[3].visible" label="领用原因" align="center" prop="getReason" width="120" show-overflow-tooltip />
 
-			<el-table-column v-if="columns[4].visible" label="物品名称" align="center" prop="itemName" width="120" show-overflow-tooltip />
+			<el-table-column v-if="columns[4].visible" label="物品名称" align="center" prop="itemName" show-overflow-tooltip />
 
 			<el-table-column v-if="columns[5].visible" label="单位" align="center" prop="unit" width="100" show-overflow-tooltip />
 
@@ -91,7 +91,7 @@
 
 			<el-table-column v-if="columns[9].visible" label="领用人" align="center" prop="handler" width="120" show-overflow-tooltip />
 
-			<el-table-column v-if="columns[10].visible" label="备注" align="center" prop="remark" width="120" show-overflow-tooltip />
+			<el-table-column v-if="columns[10].visible" label="备注" align="center" prop="remark" show-overflow-tooltip />
 
 			<el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="170" fixed="right">
 				<template #default="scope">
@@ -322,7 +322,7 @@
 					<el-table-column label="备注" align="center" prop="remark" show-overflow-tooltip />
 
 					<!-- 添加删除按钮列 -->
-					<el-table-column label="操作" align="center" width="100">
+					<el-table-column label="操作" align="center">
 						<template #default="scope">
 							<el-button size="mini" type="text" icon="el-icon-delete" @click="handleDeleteReInDetail(scope.row)">删除</el-button>
 						</template>
