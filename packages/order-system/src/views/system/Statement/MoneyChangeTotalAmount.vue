2aec8122ecdb2851a830ecb2b7114a785c3675d3
topfunplusnew <ming10655@outlook.com>
2026-01-11 17:01:51 +0800

    Refactor: Rename inventory label for consistency and clarity

--- packages/order-system/src/views/system/Statement/MoneyChangeTotalAmount.vue
+++ packages/order-system/src/views/system/Statement/MoneyChangeTotalAmount.vue
@@ -289,7 +289,7 @@ export default {
 				createRow('⑤其他应收-个人从公司借款', startTimeMoney.loanFromCompany, endTimeMoney.loanFromCompany, data.loanFromCompany, `loanFromCompany`),
 				createRow('⑥期货保证金', startTimeMoney.futuresMarginBalance, endTimeMoney.futuresMarginBalance, data.futuresMarginBalance, `futuresMarginBalance`),
 				createRow('⑦支付保证金', startTimeMoney.paymentMarginBalance, endTimeMoney.paymentMarginBalance, data.paymentMarginBalance, `paymentMarginBalance`),
-				createRow('⓪零号库存金额', startTimeMoney.remainingInventoryAmount, endTimeMoney.remainingInventoryAmount, data.remainingInventoryAmount, `remainingInventoryAmount`),
+				createRow('⓪库存金额', startTimeMoney.remainingInventoryAmount, endTimeMoney.remainingInventoryAmount, data.remainingInventoryAmount, `remainingInventoryAmount`),
 				createRow('③欠厂家货款', startTimeMoney.supplierTotalBalance, endTimeMoney.supplierTotalBalance, data.supplierTotalBalance, `supplierTotalBalance`),
 				createRow('④未支付运费合计', startTimeMoney.driverUnpaidAmount, endTimeMoney.driverUnpaidAmount, data.driverUnpaidAmount, `driverUnpaidAmount`),
 				createRow('⑧收取保证金', startTimeMoney.receiveMarginBalance, endTimeMoney.receiveMarginBalance, data.receiveMarginBalance, `receiveMarginBalance`),
