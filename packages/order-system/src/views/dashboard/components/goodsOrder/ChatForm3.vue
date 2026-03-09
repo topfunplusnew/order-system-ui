aec2ab4c1f4f3ceba366b53200301395f4f55e98
topfun <ming10655@gmail.com>
2026-03-09 23:48:26 +0800

    调整聊天表单底部注释的样式，减少上边距以提升界面美观性。

--- packages/order-system/src/views/dashboard/components/goodsOrder/ChatForm3.vue
+++ packages/order-system/src/views/dashboard/components/goodsOrder/ChatForm3.vue
@@ -454,7 +454,7 @@ export default {
 				</tbody>
 			</table>
 
-			<div class="footer-note" style="margin-top: 40px; padding: 20px 0; font-size: 16px; line-height: 2.5">
+			<div class="footer-note" style="margin-top: 5px; padding: 20px 0; font-size: 16px; line-height: 2.5">
 				<p style="margin: 5px 0">注：</p>
 				<p style="margin: 5px 0">1. 玻璃为易碎品，请当面验货，出现问题由司当面解决，收货后出现一切质量问题，由客户自负，我公司概不负责。</p>
 				<p style="margin: 5px 0">2. 此单据等同合同，客户收货后具有法律效力，若发生经济纠纷，由供货方所在地法庭处理。</p>
